8b5009cd331b6e417a83f54fecdb67d363062071
Rick Alves <henryworkdev.ti@gmail.com>
2025-03-24 22:03:57 -0300

    atualizando auth

--- src/auth/supabase-client.provider.ts
+++ src/auth/supabase-client.provider.ts
@@ -6,6 +6,7 @@ export const SUPABASE_CLIENT = 'SUPABASE_CLIENT';
 
 export const supabaseClientProvider = {
   provide: SUPABASE_CLIENT,
+  inject: [ConfigService],
   useFactory: (configService: ConfigService): SupabaseClient => {
     const supabaseUrl = configService.get<string>('SUPABASE_URL') as string;
     const supabaseKey = configService.get<string>(
@@ -13,5 +14,4 @@ export const supabaseClientProvider = {
     ) as string;
     return createClient(supabaseUrl, supabaseKey);
   },
-  inject: [ConfigService],
 };
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
-/* eslint-disable @typescript-eslint/no-floating-promises */
+// src/main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { Reflector } from '@nestjs/core';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  // Aplica o JwtAuthGuard global
+  const reflector = app.get(Reflector);
+  app.useGlobalGuards(new JwtAuthGuard(reflector));
+
+  await app.listen(3000);
 }
-bootstrap();
+void bootstrap();
