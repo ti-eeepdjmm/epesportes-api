f2e308655f5c1c635e4eeca5f6ad09ff59dae256
Rick Alves <henryworkdev.ti@gmail.com>
2025-04-05 11:48:56 -0300

    atualizando Auth

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,6 @@ async function bootstrap() {
   const reflector = app.get(Reflector);
   app.useGlobalGuards(new JwtAuthGuard(reflector));
 
-  await app.listen(3000);
+  await app.listen(3000, '0.0.0.0');
 }
 void bootstrap();
